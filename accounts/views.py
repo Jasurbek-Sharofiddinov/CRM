b4bcb20b68d0e5e1012323d26598a7b97e29a04a
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-10-04 13:44:30 +0500

    user profile,upload image

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import *
 from .filters import OrderFilter
 from .decorators import *
 
@@ -91,6 +91,17 @@ def userPage(request):
 	           }
 	return render( request, 'accounts/user.html', context )
 
+@login_required( login_url='login' )
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+	customer=request.user.customer
+	form=CustomerForm(instance=customer)
+	if request.method=='POST':
+		form = CustomerForm( request.POST, request.FILES,instance=customer )
+		if form.is_valid():
+			form.save()
+	context={'form': form}
+	return render(request,'accounts/account_settings.html',context)
 
 @login_required( login_url='login' )
 @allowed_users(allowed_roles=['admin'])
--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'accounts',
     'django_filters',
+
 ]
 
 MIDDLEWARE = [
@@ -129,6 +131,9 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
